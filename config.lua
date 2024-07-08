21069f999001df258d9a4869e649f1b026b00e7e
XobotDev <67854502+XobotDev@users.noreply.github.com>
2024-07-08 23:09:41 +1000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,10 +1,10 @@
-Config = {}
+Config = {
     -- Debug System
-    BthDebug = false -- Default: False
+    BthDebug = false, -- Default: False
     
     -- BTH Config
     BTHItemToGive = "bth-baggy", -- Item to give after the process
     BTHItemToTake1 = "poppy_plant", -- First item to take before processing
     BTHItemToTake2 = "oil-barrel", -- Second item to take before processing
-    BTHCoords = vector3(-885.51, 7621.87, 41.49), -- Coords
-
+    BTHCoords = vector3(-885.51, 7621.87, 41.49) -- Coords
+}
